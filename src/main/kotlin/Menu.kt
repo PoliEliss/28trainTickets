206b86a62340d02b3bcf8ff5da1ef45b6cfd49d4
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-03-28 18:28:49 +0300

    add fun returnTicket

--- src/main/kotlin/Menu.kt
+++ src/main/kotlin/Menu.kt
@@ -1,3 +1,18 @@
+import java.util.*
+
 class Menu (){
+
+
+     fun  returnTicket(){
+
+         val scanner = Scanner(System.`in`)
+         println("Введите номер билета для возврата")
+         val numberAnswer = scanner.nextLine()
+
+         if (numberAnswer != null  && numberAnswer != ""){
+             println("Ваше заявление будет расмотрено. Мы вернем деньги в течение 10 рабочих л")
+         }
+     }
+
 
 }
